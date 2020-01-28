1b4726bf537fa9c4ff7a2ee5ead851d7c5bbc8d8
tomiiha <52747351+tomiiha@users.noreply.github.com>
2020-01-28 16:03:45 +0000

    Update bin_con.py

--- bin_con.py
+++ bin_con.py
@@ -1,13 +1,22 @@
 # How many bits:
-bits = 6
 bits_list = []
 bit_add = 1
 
+# User bits input
+while True:
+    try:
+        bit_input = input("Input total bits: ")
+        bit_input = int(bit_input)
+        break
+    except ValueError:
+        print("Input a number.")
+        continue
+    
 # User binary input.
 while True:
     try:
-        binary = input("Input binary up to " + str(bits) + " bits: ")
-        int(binary)
+        binary = input("Input binary up to " + str(bit_input) + " bits: ")
+        binary = int(binary)
         break
     except ValueError:
         print("Input only 1s and 0s.")
@@ -16,8 +25,9 @@ while True:
 # Convert that to something we can measure in length.
 binary = str(binary)
 count_bin = len(binary)
+bits = count_bin
 
-if bits > count_bin or bits < count_bin:
+if bit_input > count_bin or bit_input < count_bin:
     print("Expected bits: " + str(bits) + ", input was " + str(count_bin))
     print("Please adjust binary to " + str(count_bin) + " bits")
 
@@ -25,3 +35,6 @@ while bits > 0:
     bits_list.append(bit_add)
     bit_add = bit_add * 2
     bits = bits - 1
+
+# Show bit numbers
+print(bits_list)
