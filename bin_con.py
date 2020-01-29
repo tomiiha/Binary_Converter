988ad509c04cfc9ae484ef199750215026f093f8
tomiiha <52747351+tomiiha@users.noreply.github.com>
2020-01-29 15:44:49 +0000

    Update bin_con.py

--- bin_con.py
+++ bin_con.py
@@ -1,8 +1,9 @@
-# How many bits:
+# How many bits.
 bits_list = []
+binary_list = []
 bit_add = 1
 
-# User bits input
+# User bits input.
 while True:
     try:
         bit_input = input("Input total bits: ")
@@ -23,17 +24,25 @@ while True:
         continue
 
 # Convert that to something we can measure in length.
-binary = str(binary)
-count_bin = len(binary)
+count_bin = len(str(binary))
 
 if bit_input > count_bin or bit_input < count_bin:
     print("Expected bits: " + str(count_bin) + ", input was " + str(count_bin))
     print("Please adjust binary to " + str(count_bin) + " bits")
 
+# Create bit instances.
 while count_bin > 0:
     bits_list.append(bit_add)
     bit_add = bit_add * 2
     count_bin = count_bin - 1
 
+# Sort binary into list.
+for var in str(binary):
+    binary_list.append(var)
+
+# Reverse to assign bit ordering.
+bits_list.reverse()
+
 # Show bit numbers
 print(bits_list)
+print(binary_list)
